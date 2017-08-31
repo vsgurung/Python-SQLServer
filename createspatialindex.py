b8fa5e81ef9ac00aa7fd96d17d3ddc47431b8bfd
vsgurung <vsgurung@yahoo.com>
2017-08-31 20:51:34 +0100

    Updated the script.
    Renamed functions and changed the logic.

--- createspatialindex.py
+++ createspatialindex.py
@@ -5,7 +5,6 @@
 # Importing the required modules
 import pyodbc
 
-
 def create_cursor():
 	"""
 	Description: This function returns a cursor object.
@@ -14,33 +13,29 @@ def create_cursor():
 	"""
 	user='username'
 	pw='password'
-	try:
-		with pyodbc.connect(driver='{ODBC Driver 13 for SQL Server}',host='databaseservername',database='databasename',UID=user,PWD=pw) as conn:
-			cur = conn.cursor()
-			#print('All is Well. Returning Cursor object')
+	with pyodbc.connect(driver='{ODBC Driver 13 for SQL Server}',host='databaseservername',database='databasename',UID=user,PWD=pw) as conn:
+		cur = conn.cursor()
+		#print('All is Well. Returning Cursor object')
+		if cur:
 			return cur
-	except pyodbc.Error as e:
-		print(e[1])
 
-
+'''		
 def get_tables(in_cursor,database='databasename',schema='dbo'):
 	"""
 	Description: Returns list of tables in a database.
 	Parameters: cursor object, database name and schema name
 	Returns: List of all table names
 	"""
-	try:
-		table_list = []
-		for row in in_cursor.tables(catalog=database,schema=schema):
-			table_list.append(row.table_name)
-		return table_list
-	except pyodbc.Error as e:
-		print(e[1])
-
+	table_list = []
+	for row in in_cursor.tables(catalog=database,schema=schema):
+		table_list.append(row.table_name)
+		if table_list:
+			return table_list
+'''
 
 def get_spatial_tables(in_cursor, schema='dbo'):
 	"""
-	Description: This table returns list of all spatial tables.
+	Description: This table returns list of all spatial tables (geometry).
 	Parameters, accepts two arguments cursor and schema name
 	Returns: List of table
 	"""	
@@ -49,98 +44,97 @@ def get_spatial_tables(in_cursor, schema='dbo'):
         AND t.TABLE_TYPE = 'BASE TABLE' where c.DATA_TYPE = 'geometry'
          and c.TABLE_SCHEMA=?""",schema)
 	spatial_tables = [s[0] for s in in_cursor.fetchall()]
-	return spatial_tables
+	if spatial_tables:
+		return spatial_tables
 
 
-def check_primarykey(in_cur, in_table):
-	"""
-	Description: This function checks if table has primary key and returns boolean
-	Parameters: cursor object and table
-	Returns: Boolean
-	"""
-	try:
-		for r in in_cur.primaryKeys(table=in_table):
-			if r.pk_name:
-				return True
-			else:
-				return False
-	except pyodbc.Error as e:
-		print(e[1])
-
-
-def check_spatialindex(in_cur, in_table):
+def has_spatialindex(in_cur, in_table):
 	"""
 	Description: This function checks if table has spatial index
 	Parameters: cursor and table
 	Returns: Boolean
 	"""
-	try:
-		in_cur.execute("""select * from sys.indexes 
+	in_cur.execute("""select * from sys.indexes 
 							where object_id = (select object_id from sys.objects where name = ?) 
 							and type_desc='SPATIAL'""",in_table)
-		r = [v[1] for v in in_cur.fetchall()]
-		#print(r)
-		if not r:
-			return False
-		else:
+	
+	r = [v[1] for v in in_cur.fetchall()] # Getting the table names only
+	#print(r)
+	if not r:
+		return False
+	else:
+		return True
+
+
+def has_primarykey(in_cur, in_table):
+	"""
+	Description: This function checks if table has primary key and returns boolean
+	Parameters: cursor object and table
+	Returns: True if table has primary key
+	"""
+	for r in in_cur.primaryKeys(table=in_table):
+		if r.pk_name:
 			return True
-	except pyodbc.Error as e:
-		print(e[1])
+		else:
+			return False
 
-
+	
 def get_boundingbox(in_cur, in_table):
 	"""
 	Description: This function returns dictionary of bounding box coordinates. 
 	Parameters: cursor and table
 	Returns: Dictionary of bounding box coordinates
 	"""
-	try:
-
-		key_list = ['MinX','MinY','MaxX','MaxY']
-		in_cur.execute("""SELECT
-			geometry::EnvelopeAggregate(GEOM).STPointN(1).STX AS MinX,
-			geometry::EnvelopeAggregate(GEOM).STPointN(1).STY AS MinY,
-			geometry::EnvelopeAggregate(GEOM).STPointN(3).STX AS MaxX,
-			geometry::EnvelopeAggregate(GEOM).STPointN(3).STY AS MaxY
-			FROM {}""".format(in_table))
-		coords = cur.fetchall()
-		value_list = [coord for coord in coords[0]]
+	key_list = ['MinX','MinY','MaxX','MaxY']
+	in_cur.execute("""SELECT
+		geometry::EnvelopeAggregate(GEOM).STPointN(1).STX AS MinX,
+		geometry::EnvelopeAggregate(GEOM).STPointN(1).STY AS MinY,
+		geometry::EnvelopeAggregate(GEOM).STPointN(3).STX AS MaxX,
+		geometry::EnvelopeAggregate(GEOM).STPointN(3).STY AS MaxY
+		FROM {}""".format(in_table))
+	coords = cur.fetchall()
+	value_list = [coord for coord in coords[0]]
+	if value_list:
 		return dict(zip(key_list,value_list))
-	except pyodbc.Error as e:
-		print(e[1])
 
-def create_spatialindex(in_cur, in_table):
+
+def create_spatialindex(in_cur, in_table,in_boundingbox):
 	"""
 	Description: This function creates spatial index on a table. 
-	Parameters: Accepts two arguments cursor and table.
+	Parameters: Accepts three arguments cursor and table.
 	Doesn't return any object. Remember to commit if autocommit is not set in the connection object. 
 	"""
-	try:
-		bb=get_boundingbox(in_cur, in_table)
-		#print(bb)
-		print('Creating Spatial Index for {}'.format(in_table))
-		in_cur.execute("""CREATE SPATIAL INDEX SIDX_{} ON {}(GEOM)
-						USING GEOMETRY_GRID WITH(
-						BOUNDING_BOX=(xmin={},ymin={},xmax={},ymax={}),
-						GRIDS=(MEDIUM, MEDIUM, MEDIUM, MEDIUM),
-						CELLS_PER_OBJECT=16) """.format(in_table,in_table,bb['MinX'],bb['MinY'],bb['MaxX'],bb['MaxY']))
-		in_cur.commit()
-		print('Spatial Index for {} created successfully.'.format(in_table))
-	except pyodbc.Error as e:
-		print(e[1])
+	print('Creating Spatial Index for {}'.format(in_table))
+	in_cur.execute("""CREATE SPATIAL INDEX SIDX_{} ON {}(GEOM)
+					USING GEOMETRY_GRID WITH(
+					BOUNDING_BOX=(xmin={},ymin={},xmax={},ymax={}),
+					GRIDS=(MEDIUM, MEDIUM, MEDIUM, MEDIUM),
+					CELLS_PER_OBJECT=16) """.format(in_table,in_table,bb['MinX'],bb['MinY'],bb['MaxX'],bb['MaxY']))
+	in_cur.commit()
+	print('Spatial Index for {} created successfully.'.format(in_table))
 
 
-cur = create_cursor()
+try:
+	cur = create_cursor('BHOOTNATH-PC\SQLEXPRESS', 'OS_SampleData', 'vsgurung', 'admingurung')
+	tables = get_spatial_tables(cur)
 
-tables = get_spatial_tables(cur)
+	for t in sorted(tables):
+		has_si = has_spatialindex(cur, t)
+		#print(has_si)
+		try:
+			if not has_si:
+				has_pk=has_primarykey(cur, t)
+				if has_pk:
+					bb = get_boundingbox(cur, t)
+					create_spatialindex(cur, t, bb)
+				else:
+					#set_primarykey()
+					print('Table {} has no primary key defined.'.format(t))
+			else:
+				print('Table {} already has a spatial index.'.format(t))
+		except pyodbc.Error as e:
+			print(e[1])
 
-for t in sorted(tables):
-	check_si = check_spatialindex(cur, t)
-	try:
-		if not check_si:
-			create_spatialindex(cur, t)
-		else:
-			print('Table {} has spatial index.'.format(t))
-	except pyodbc.Error as e:
-		print(e[1])
+except Exception as e:
+	print(e)
 
